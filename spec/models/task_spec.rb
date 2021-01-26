f5b1b60b794712ebbafa06f6cfa766fa93e86e5b
Hiromu Sato <a.string.d@gmail.com>
2021-01-26 19:04:10 +0900

    テストの記述

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+    it 'タイトルが無ければ無効な状態であること' do
+      user = FactoryBot.create(:user)
+
+      task = user.tasks.new(
+        title: nil,
+        content: "hogehoge",
+        status: "todo",
+        deadline: Time.zone.now
+      )
+      task.valid?
+      expect(task.errors[:title]).to include("can't be blank")
+    end
+    it 'ステータスが無ければ無効な状態であること' do
+      user = FactoryBot.create(:user)
+
+      task = user.tasks.new(
+        title: "example",
+        content: "hogehoge",
+        status: nil,
+        deadline: Time.zone.now
+      )
+      task.valid?
+      expect(task.errors[:status]).to include("can't be blank")
+    end
+    
+    # it 'タイトルがユニークでなければ無効な状態であること' do
+    # end
+    # it 'ステータスがなければ無効な状態であること' do
+    # end
 end
