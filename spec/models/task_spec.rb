13803b77ed6d58ed7d90f92ea908c28a089860f3
Hiromu Sato <a.string.d@gmail.com>
2021-01-27 11:01:59 +0900

    テストの追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,7 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-    it 'タイトルが無ければ無効な状態であること' do
+    it '有効であること' do
+      user = FactoryBot.create(:user)
+      task = user.tasks.create(
+        title: "example",
+        content: "hogehoge",
+        status: "todo",
+        deadline: Time.zone.now
+      )
+      expect(task).to be_valid
+    end
+    it 'タイトルが無ければ無効であること' do
       user = FactoryBot.create(:user)
 
       task = user.tasks.new(
@@ -14,7 +24,7 @@ RSpec.describe Task, type: :model do
       task.valid?
       expect(task.errors[:title]).to include("can't be blank")
     end
-    it 'ステータスが無ければ無効な状態であること' do
+    it 'ステータスが無ければ無効であること' do
       user = FactoryBot.create(:user)
 
       task = user.tasks.new(
@@ -43,5 +53,22 @@ RSpec.describe Task, type: :model do
       )
       task2.valid?
       expect(task2.errors[:title]).to include("has already been taken")
+    end
+    it 'タイトルが一意であれば有効であること' do
+      user = FactoryBot.create(:user)
+
+      task1 = user.tasks.create(
+        title: "example",
+        content: "hogehoge",
+        status: "todo",
+        deadline: Time.zone.now
+      )
+      task2 = user.tasks.create(
+        title: "example2",
+        content: "hogehoge",
+        status: "todo",
+        deadline: Time.zone.now
+      )
+      expect(task2).to be_valid
     end
 end
