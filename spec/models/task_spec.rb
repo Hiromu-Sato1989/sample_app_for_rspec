11a34278c966dc62620052bf44991aa7a2d94220
Hiromu Sato <a.string.d@gmail.com>
2021-01-27 17:09:06 +0900

    Fix: テスト失敗時の記述

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -7,28 +7,29 @@ RSpec.describe Task, type: :model do
       # user = FactoryBot.create(:user)
       task = build(:task)
       expect(task).to be_valid
+      expect(task.errors).to be_empty
     end
     it 'タイトルが無ければ無効であること' do
       # user = FactoryBot.create(:user)
 
-      task = build(:task, title: nil)
-      task.valid?
-      expect(task.errors[:title]).to include("can't be blank")
+      task_without_title = build(:task, title: nil)
+      expect(task_without_title).to be_invalid
+      expect(task_without_title.errors[:title]).to include("can't be blank")
     end
     it 'ステータスが無ければ無効であること' do
       # user = FactoryBot.create(:user)
 
-      task = build(:task, status: nil)
-      task.valid?
-      expect(task.errors[:status]).to include("can't be blank")
+      task_without_status = build(:task, status: nil)
+      expect(task_without_status).to be_invalid
+      expect(task_without_status.errors[:status]).to include("can't be blank")
     end
     it 'タイトルが一意でなければ無効な状態であること' do
       # user = FactoryBot.create(:user)
 
       task = create(:task)
-      task_with_another_title = build(:task, title: task.title)
-      task_with_another_title.valid?
-      expect(task_with_another_title.errors[:title]).to include("has already been taken")
+      task_with_duplicated_title = build(:task, title: task.title)
+      expect(task_with_duplicated_title).to be_invalid
+      expect(task_with_duplicated_title.errors[:title]).to include("has already been taken")
     end
     it 'タイトルが一意であれば有効であること' do
       # user = FactoryBot.create(:user)
@@ -36,6 +37,7 @@ RSpec.describe Task, type: :model do
       task = create(:task)
       task_with_another_title = build(:task, title: 'another_title')
       expect(task_with_another_title).to be_valid
+      expect(task_with_another_title.errors).to be_empty
     end
   end
 end
