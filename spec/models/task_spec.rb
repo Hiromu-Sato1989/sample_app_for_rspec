56f6b394dac2561f0578ae969595f92e6dd28d4e
Hiromu Sato <a.string.d@gmail.com>
2021-01-27 17:00:36 +0900

    Fix: FactoryBot.の記載を省略

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,36 +5,36 @@ RSpec.describe Task, type: :model do
   describe 'validates' do
     it '有効であること' do
       # user = FactoryBot.create(:user)
-      task = FactoryBot.build(:task)
+      task = build(:task)
       expect(task).to be_valid
     end
     it 'タイトルが無ければ無効であること' do
       # user = FactoryBot.create(:user)
 
-      task = FactoryBot.build(:task, title: nil)
+      task = build(:task, title: nil)
       task.valid?
       expect(task.errors[:title]).to include("can't be blank")
     end
     it 'ステータスが無ければ無効であること' do
       # user = FactoryBot.create(:user)
 
-      task = FactoryBot.build(:task, status: nil)
+      task = build(:task, status: nil)
       task.valid?
       expect(task.errors[:status]).to include("can't be blank")
     end
     it 'タイトルが一意でなければ無効な状態であること' do
       # user = FactoryBot.create(:user)
 
-      task = FactoryBot.create(:task)
-      task_with_another_title = FactoryBot.build(:task, title: task.title)
+      task = create(:task)
+      task_with_another_title = build(:task, title: task.title)
       task_with_another_title.valid?
       expect(task_with_another_title.errors[:title]).to include("has already been taken")
     end
     it 'タイトルが一意であれば有効であること' do
       # user = FactoryBot.create(:user)
 
-      task = FactoryBot.create(:task)
-      task_with_another_title = FactoryBot.create(:task, title: 'another_title')
+      task = create(:task)
+      task_with_another_title = build(:task, title: 'another_title')
       expect(task_with_another_title).to be_valid
     end
   end
