f1fd1cb6a0c247c362b2ff62d6d7f7915aedbbeb
Hiromu Sato <a.string.d@gmail.com>
2021-01-27 16:49:23 +0900

    Fix: FactoryBot.createを代入したローカル変数を利用

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,72 +5,37 @@ RSpec.describe Task, type: :model do
   describe 'validates' do
     it '有効であること' do
       user = FactoryBot.create(:user)
-      task = user.tasks.create(
-        title: "example",
-        content: "hogehoge",
-        status: "todo",
-        deadline: Time.zone.now
-      )
+      task = FactoryBot.build(:task, user: user)
       expect(task).to be_valid
     end
     it 'タイトルが無ければ無効であること' do
       user = FactoryBot.create(:user)
 
-      task = user.tasks.new(
-        title: nil,
-        content: "hogehoge",
-        status: "todo",
-        deadline: Time.zone.now
-      )
+      task = FactoryBot.build(:task, title: nil, user: user)
       task.valid?
       expect(task.errors[:title]).to include("can't be blank")
     end
     it 'ステータスが無ければ無効であること' do
       user = FactoryBot.create(:user)
 
-      task = user.tasks.new(
-        title: "example",
-        content: "hogehoge",
-        status: nil,
-        deadline: Time.zone.now
-      )
+      task = FactoryBot.build(:task, status: nil, user: user)
       task.valid?
       expect(task.errors[:status]).to include("can't be blank")
     end
     it 'タイトルが一意でなければ無効な状態であること' do
       user = FactoryBot.create(:user)
 
-      task = user.tasks.create(
-        title: "example",
-        content: "hogehoge",
-        status: "todo",
-        deadline: Time.zone.now
-      )
-      task_with_another_title = user.tasks.create(
-        title: "example",
-        content: "hogehoge",
-        status: "todo",
-        deadline: Time.zone.now
-      )
+      task = FactoryBot.create(:task, user: user)
+      task_with_another_title = FactoryBot.build(:task, title: task.title, user: user)
       task_with_another_title.valid?
       expect(task_with_another_title.errors[:title]).to include("has already been taken")
     end
     it 'タイトルが一意であれば有効であること' do
       user = FactoryBot.create(:user)
 
-      task1 = user.tasks.create(
-        title: "example",
-        content: "hogehoge",
-        status: "todo",
-        deadline: Time.zone.now
-      )
-      task2 = user.tasks.create(
-        title: "example2",
-        content: "hogehoge",
-        status: "todo",
-        deadline: Time.zone.now
-      )
-      expect(task2).to be_valid
+      task = FactoryBot.create(:task, user: user)
+      task_with_another_title = FactoryBot.create(:task, title: 'another_title', user: user)
+      expect(task_with_another_title).to be_valid
     end
   end
 end
